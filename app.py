6ddd9b8fb8d64e5087690f4f9d8fc3c49ad8d620
Kzma1 <71949462+Kzma1@users.noreply.github.com>
2022-01-12 20:36:48 +0900

    完成

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import logging
 import json
-from pprint import pprint
 from urllib.request import urlopen
 from random import shuffle
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     """初期画面を表示します."""
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    """初期画面を表示します."""
+    return render_template("index.html")
 
-# @app.route("/api/recommend_article")
+@app.route("/api/recommend_article")
 def api_recommend_article():
-#     """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
+    """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
 
     # 1. はてブのホットエントリーページのHTMLを取得する
     with urlopen("http://feeds.feedburner.com/hatena/b/hotentry") as res:
@@ -24,12 +23,9 @@ def api_recommend_article():
     soup = BeautifulSoup(html, "html.parser")
     # 3. 記事一覧を取得する
     titles = soup.select("item")
-    # t.stringでなにかできる？ → タグの中身の要素だけを取り出す
-    # titles = [t for t in titles]
     # 4. ランダムに1件取得する
     shuffle(titles)
     title = titles[0]
-    # pprint(titles[0])
     # 5. 以下の形式で返却する.
     #     {
     #         "content" : "記事のタイトル",
@@ -39,7 +35,6 @@ def api_recommend_article():
         "content" : title.find("title").string,
         "link" : title.get('rdf:about')
     })
-api_recommend_article()
 
 # @app.route("/api/xxxx")
 # def api_xxxx():
@@ -51,5 +46,5 @@ api_recommend_article()
 #     """
 #     pass
 
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5004)
+if __name__ == "__main__":
+    app.run(debug=True, port=5004)
