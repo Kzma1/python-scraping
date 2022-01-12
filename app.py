690edcf463b30427e4de54484185f9b031f3423d
Kzma1 <71949462+Kzma1@users.noreply.github.com>
2022-01-12 20:46:52 +0900

    条件分岐作成

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 import json
+import random
 from urllib.request import urlopen
-from random import shuffle
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
@@ -23,14 +23,22 @@ def api_recommend_article():
     soup = BeautifulSoup(html, "html.parser")
     # 3. 記事一覧を取得する
     titles = soup.select("item")
-    # 4. ランダムに1件取得する
-    shuffle(titles)
-    title = titles[0]
-    # 5. 以下の形式で返却する.
+    # 4. ランダムに1件取得し以下の形式で返却する.
     #     {
     #         "content" : "記事のタイトル",
     #         "link" : "記事のURL"
     #     }
+
+    index = random.randrange(100)
+
+    if index >= len(titles):
+        logging.error('error!')
+        return json.dumps( {
+            "content" : "記事のタイトル",
+            "link" : "記事のURL"
+        })
+
+    title = titles[index]
     return json.dumps({
         "content" : title.find("title").string,
         "link" : title.get('rdf:about')
