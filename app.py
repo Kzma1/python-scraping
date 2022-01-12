437113db32df4fc5558b56e19b94a84b049fd7a4
Kzma1 <71949462+Kzma1@users.noreply.github.com>
2022-01-12 11:10:12 +0900

    記事内容取得

--- app.py
+++ app.py
@@ -1,49 +1,56 @@
+import logging
 import json
+from pprint import pprint
 from urllib.request import urlopen
 from random import shuffle
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def index():
-    """初期画面を表示します."""
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     """初期画面を表示します."""
+#     return render_template("index.html")
 
-@app.route("/api/recommend_article")
+# @app.route("/api/recommend_article")
 def api_recommend_article():
-    """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
+#     """はてブのホットエントリーから記事を入手して、ランダムに1件返却します."""
 
-    """
-        **** ここを実装します（基礎課題） ****
+    # 1. はてブのホットエントリーページのHTMLを取得する
+    with urlopen("http://feeds.feedburner.com/hatena/b/hotentry") as res:
+        html = res.read().decode("utf-8")
+    # 2. BeautifulSoupでHTMLを読み込む
+    soup = BeautifulSoup(html, "html.parser")
+    # 3. 記事一覧を取得する
+    titles = soup.select("item")
+    titles = [t.string for t in titles]
+    # 4. ランダムに1件取得する
+    index = shuffle
+    print(index)
+    pprint(titles[index])
+    # 5. 以下の形式で返却する.
+    #     {
+    #         "content" : "記事のタイトル",
+    #         "link" : "記事のURL"
+    #     }
 
-        1. はてブのホットエントリーページのHTMLを取得する
-        2. BeautifulSoupでHTMLを読み込む
-        3. 記事一覧を取得する
-        4. ランダムに1件取得する
-        5. 以下の形式で返却する.
-            {
-                "content" : "記事のタイトル",
-                "link" : "記事のURL"
-            }
-    """
-
+api_recommend_article()
     # ダミー
-    return json.dumps({
-        "content" : "記事のタイトルだよー",
-        "link" : "記事のURLだよー"
-    })
+    # return json.dumps({
+    #     "content" : "記事のタイトルだよー",
+    #     "link" : "記事のURLだよー"
+    # })
 
-@app.route("/api/xxxx")
-def api_xxxx():
-    """
-        **** ここを実装します（発展課題） ****
-        ・自分の好きなサイトをWebスクレイピングして情報をフロントに返却します
-        ・お天気APIなども良いかも
-        ・関数名は適宜変更してください
-    """
-    pass
+# @app.route("/api/xxxx")
+# def api_xxxx():
+#     """
+#         **** ここを実装します（発展課題） ****
+#         ・自分の好きなサイトをWebスクレイピングして情報をフロントに返却します
+#         ・お天気APIなども良いかも
+#         ・関数名は適宜変更してください
+#     """
+#     pass
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5004)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5004)
